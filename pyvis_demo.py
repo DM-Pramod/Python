db9e552335144ba0f9f9e7eaaf9aa094e280fbe1
Pramod DM <58771714+DM-Pramod@users.noreply.github.com>
2024-10-11 21:50:54 +0530

    Update pyvis_demo.py

--- pyvis_demo.py
+++ pyvis_demo.py
@@ -1,9 +1,14 @@
 from pyvis.network import Network as GNetwork
+
 import json
+
 from jinja2 import Environment, FileSystemLoader
+
 from config import Config 
 
+
 class Visual():
+
     def create_visual(self, file_name=None):
         self.nds = []
         self.eds = []
@@ -23,7 +28,7 @@ class Visual():
 
             for node in self.nodes:
                 type = node.type 
-                if node.type == 'term_source' :
+                if node.type == 'term_source':
                     type = 'source'
                 elif node.type == 'term_sink':
                     type = 'sink'
@@ -31,31 +36,23 @@ class Visual():
                 G.add_node(
                     node.node_id,
                     shape="dot",
-                    label="{} Node ={}\n f_i={}\n t_i={}".format(node.type, node.node_id,node.window_start, node.window_end,),
+                    label="{} Node ={}\n f_i={}\n t_i={}".format(node.type, node.node_id, node.window_start, node.window_end),
                     color=(
                         self.config.COLOR[type]
                         if type != "out-charters"
                         else self.config.COLOR_OUT_CHARTERS[type]
                     ),
                     title=f"""
-                        <table style="width:100%; border-collapse: collapse;">
-                            <tr>
-                                <th style="text-align: left; padding: 5px;">Type</th>
-                                <th style="text-align: left; padding: 5px;">Node ID</th>
-                                <th style="text-align: left; padding: 5px;">From</th>
-                                <th style="text-align: left; padding: 5px;">To</th>
-                            </tr>
-                            <tr>
-                                <td style="text-align: left; padding: 5px;">{node.type}</td>
-                                <td style="text-align: left; padding: 5px;">{node.node_id}</td>
-                                <td style="text-align: left; padding: 5px;">{node.window_start}</td>
-                                <td style="text-align: left; padding: 5px;">{node.window_end}</td>
-                            </tr>
-                        </table>
+                    <table>
+                        <tr><th>Type</th><td>{node.type}</td></tr>
+                        <tr><th>Node ID</th><td>{node.node_id}</td></tr>
+                        <tr><th>From</th><td>{node.window_start}</td></tr>
+                        <tr><th>To</th><td>{node.window_end}</td></tr>
+                    </table>
                     """,
                 )
 
-                self.nds.append({"type":node.type, "id":node.node_id,"From":node.window_start, "To":node.window_end})
+                self.nds.append({"type": node.type, "id": node.node_id, "From": node.window_start, "To": node.window_end})
 
             for arc in self.arcs:
                 G.add_edge(
@@ -66,32 +63,23 @@ class Visual():
                         arc.upstr.node_id,
                         arc.dwnstr.node_id,
                         (arc.arc_upstr_window_start, arc.arc_upstr_window_end, arc.arc_dwnstr_window_start, arc.arc_dwnstr_window_end),
-                        # (arc.laden_days,arc.ballast_days,arc.waiting_days,arc.port_days)
                     ),
                     title=f"""
-                        <table style="width:100%; border-collapse: collapse;">
-                            <tr>
-                                <th style="text-align: left; padding: 5px;">Vessel</th>
-                                <th style="text-align: left; padding: 5px;">Next Node ID</th>
-                                <th style="text-align: left; padding: 5px;">From Node ID</th>
-                                <th style="text-align: left; padding: 5px;">Days</th>
-                            </tr>
-                            <tr>
-                                <td style="text-align: left; padding: 5px;">{vn}</td>
-                                <td style="text-align: left; padding: 5px;">{arc.upstr.node_id}</td>
-                                <td style="text-align: left; padding: 5px;">{arc.dwnstr.node_id}</td>
-                                <td style="text-align: left; padding: 5px;">{arc.arc_upstr_window_start}, {arc.arc_upstr_window_end}, {arc.arc_dwnstr_window_start}, {arc.arc_dwnstr_window_end}</td>
-                            </tr>
-                        </table>
+                    <table>
+                        <tr><th>Vessel</th><td>{vn}</td></tr>
+                        <tr><th>Next Node ID</th><td>{arc.upstr.node_id}</td></tr>
+                        <tr><th>From Node ID</th><td>{arc.dwnstr.node_id}</td></tr>
+                        <tr><th>Days</th><td>{arc.arc_upstr_window_start}, {arc.arc_upstr_window_end}, {arc.arc_dwnstr_window_start}, {arc.arc_dwnstr_window_end}</td></tr>
+                    </table>
                     """,
                 )
-            #******************************
-                self.eds.append({"Vessel":vn,
-                        "Next_NodeID":arc.upstr.node_id,
-                        "From_NodeID":arc.dwnstr.node_id,
-                        "DAYS":(arc.arc_upstr_window_start, arc.arc_upstr_window_end, arc.arc_dwnstr_window_start, arc.arc_dwnstr_window_end)},)
-            # print("**************")
 
+                self.eds.append({
+                    "Vessel": vn,
+                    "Next_NodeID": arc.upstr.node_id,
+                    "From_NodeID": arc.dwnstr.node_id,
+                    "DAYS": (arc.arc_upstr_window_start, arc.arc_upstr_window_end, arc.arc_dwnstr_window_start, arc.arc_dwnstr_window_end),
+                })
 
             G.repulsion(node_distance=200, spring_length=200)
             if file_name:
