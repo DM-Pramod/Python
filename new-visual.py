c8e76e4cf5d87066dc6dd6a7722521682d1356b8
Pramod DM <58771714+DM-Pramod@users.noreply.github.com>
2025-01-30 15:35:33 +0530

    Update new-visual.py

--- new-visual.py
+++ new-visual.py
@@ -1,5 +1,54 @@
-df = actualdf.assign(
-    col1=actualdf['colA'].apply(lambda x: x.split('+')[0]),
-    col2=actualdf['colA'].apply(lambda x: x.split('+')[1].split('(')[0]),
-    col3=actualdf['colA'].apply(lambda x: '(' + x.split('(')[1])
-)
+Use yfinance, streamlit and python and create a dashboard
+to the left side let there be a drop down which has all 195 countries 
+When selected a particular country It needs to pick top 1000 Cryptocurrencies which are developed in that country or originated and are very popular across.
+
+Once the 1000 Cryptos are taken create a table which has all the information related to them
+Informations like 
+1. Current trading price and few other important trading data from yfinance some 6 columns related to that
+2. Purpose or pblm statement that particular crypto trying to solve
+3. Block chain on top of which it is built
+4. Developed By(team name or developer name and origin)
+5. By Purpose and Use Case
+Currency Coins: 
+Utility Tokens:
+Security Tokens: 
+Stablecoins: 
+Governance Tokens: 
+
+Add if there is any more classification and make true false as the cell value. if you couldn't find out the information mark it NA
+
+6. By Technology and Consensus Mechanism
+Proof of Work (PoW): 
+Proof of Stake (PoS): 
+Delegated Proof of Stake (DPoS): 
+Proof of Authority (PoA): 
+Add if there is any more classification and make true false as the cell value.
+
+
+7. By Generation
+First Generation: 
+Second Generation:
+Third Generation: 
+
+
+
+8. By Market Capitalization
+Large-Cap Cryptocurrencies:
+Mid-Cap Cryptocurrencies:
+Small-Cap Cryptocurrencies:
+Add if there is any more classification and make true false as the cell value.
+
+9. By Privacy Features
+Public Cryptocurrencies: 
+Privacy Coins: 
+Add if there is any more classification and make true false as the cell value.
+
+10. By Industry Use Case
+Finance: 
+Supply Chain: 
+Healthcare: 
+Add if there is any more classification and make true false as the cell value.
+
+Keep the main headers like "By Generation", By Indsustry Use Case" and so as main column under that keep the sub topics as columns(2 level columns ) Everything should look like a table and i should have an option to download
+
+let the drop down be to the left and mid space for display
